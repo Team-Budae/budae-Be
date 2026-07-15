a86ff9a11fec54e4cd8b8d247fa910f6c0beaa25
vvineey <vvin506@naver.com>
2026-07-15 15:06:41 +0900

    [feat] 댓글 수 실제 데이터와 동기화

--- app/domains/comment/service.py
+++ app/domains/comment/service.py
@@ -2,6 +2,7 @@ from app.common.exceptions import ForbiddenError, NotFoundError
 from app.domains.comment.models import Comment
 from app.domains.comment.repository import CommentRepository
 from app.domains.comment.schemas import CommentCreate, CommentUpdate
+from app.domains.post.models import Post
 
 
 class CommentService:
@@ -12,8 +13,16 @@ class CommentService:
         return self.repository.list_by_post(post_id)
 
     def create_comment(self, payload: CommentCreate) -> Comment:
+        post = self.repository.session.get(Post, payload.post_id)
+        if not post or post.is_deleted:
+            raise NotFoundError("게시글을 찾을 수 없습니다.")
+
         comment = Comment(**payload.model_dump())
-        return self.repository.add(comment)
+        self.repository.session.add(comment)
+        post.comments = (post.comments or 0) + 1
+        self.repository.session.commit()
+        self.repository.session.refresh(comment)
+        return comment
 
     def update_comment(self, comment_id: int, payload: CommentUpdate) -> Comment:
         comment = self.repository.get_by_id(comment_id)
@@ -34,4 +43,7 @@ class CommentService:
         if comment.password != password:
             raise ForbiddenError("비밀번호가 일치하지 않습니다.")
         comment.is_deleted = 1
+        post = self.repository.session.get(Post, comment.post_id)
+        if post and not post.is_deleted:
+            post.comments = max((post.comments or 0) - 1, 0)
         self.repository.session.commit()
